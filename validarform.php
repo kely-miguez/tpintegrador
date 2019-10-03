c2e40264d3a8e110a15156c7a0cbf9cebb8f3bbe
Vane <vanne-894@outlook.es>
2019-10-03 19:47:10 -0300

    modifiqué las validaciones

--- validarform.php
+++ validarform.php
@@ -1,25 +1,25 @@
 <?php
    if(isset($_POST['submit'])) {
     if(empty($nombre)) {
-      echo "<p class='error'>*Agrega tu nombre</p>";
+      echo '<p class='error'>*Agrega tu nombre</p>';
     }
     if(empty($apellido)) {
-      echo "<p class='error'>*Agrega tu apellido</p>";
+      echo '<p class='error'>*Agrega tu apellido</p>';
    }
    if(empty($correo)) {
-     echo "<p class='error'>*Falta el correo</p>";
+     echo '<p class='error'>*Falta el correo</p>';
    }
    if(empty($usuario)) {
-     echo "<p class='error'>*Usuario no identificado</p>";
+     echo '<p class='error'>*Usuario no identificado</p>';
    }
    if(empty($clave)) {
-     echo "<p class='error'>*Introduce tu contraseña</p>";
+     echo '<p class='error'>*Introduce tu contraseña</p>';
    }
    if(empty($telefono)) {
-     echo "<p class='error'>*Agrega tu telefono</p>";
+     echo '<p class='error'>*Agrega tu telefono</p>';
    } else {
      if(!is_numeric($telefono)) {
-     echo "<p class='error'>*El telefono debe ser un numero</p>";
+     echo '<p class='error'>*El telefono debe ser un numero</p>';
    }
- }
+}
  ?>
