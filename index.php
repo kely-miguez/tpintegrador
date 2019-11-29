27d6c0a603e6172163a0e30dc0829f44fdf12a89
kely <kelymiguezflores.km@gmail.com>
2019-11-29 11:20:57 -0300

    se creo la carpeta plantilla

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
+<html>
   <head>
     <meta charset="utf-8">
     <title>DAYROSS</title>
@@ -7,58 +6,16 @@
        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
        crossorigin="anonymous"></script>
        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-   <link rel="stylesheet" href="CSS/stylos.css">
-   <link rel="stylesheet" href="CSS/trabajo.css">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+     <link rel="stylesheet" href="CSS/stylos.css">
   </head>
   <body>
-<div class="container">
-</div>
-<header>
-  <ul class="nav nav-tabs">
-  <li class="nav-item">
-    <a class="nav-link active" href="home.php">Home</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link active" href="preguntas.php">F.A.Q.</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link active" href="registro.php">Registro</a>
-  </li>
-  <li class="nav-item">
-  <a class="nav-link active" href="login.php">Iniciar sesion</a>
-  </li>
-  <li class="nav-item">
-<a class="nav-link active" href="tp.php">Contacto</a>
-  </li>
-<li class="nav-item">
-  <a class="nav-link active" href="perfil.php">Perfil</a>
-</li>
-</ul>
 
-<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="img/portada1.png" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="img/portada2.png" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="img/portada3.png" class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-</div>
-<br>
+<?php
+  require_once('plantilla/menu.php');
+
+ ?>
+
 <br>
 <h1>PRODUCTOS</h1>
  <section>
