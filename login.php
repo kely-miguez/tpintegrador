1abafb3e4c15699f4f57b9489bff5c1b7a9e9eed
Vane <vanne-894@outlook.es>
2019-12-03 13:56:28 -0300

    cambios

--- login.php
+++ login.php
@@ -1,3 +1,51 @@
+<?php
+session_start();
+
+  if($_POST){
+    $errores=[];
+    $usuario=[];
+    $_SESSION['selogeo']="";
+    if($_POST['email']!=''){
+      $email=$_POST['email'];
+    }
+    else{
+      $errores[]="Ingrese su email";
+    }
+    if($_POST['password']!=''){
+      $password=$_POST['password'];
+    }
+    else{
+      $errores[]="Ingrese su contraseña";
+    }
+    if(empty($errores)){
+    $usuariosjson=FILE_GET_CONTENTS('usuario.json');
+    $datosusuarios=json_decode($usuariosjson,true);
+
+    foreach ($datosusuarios as $usuarios) {
+      if($usuarios['email']==$email && password_verify($password, $usuarios['password'])){
+        $_SESSION['email']=$email;
+        $_SESSION['nombres']=$usuarios['nombres'];
+        $_SESSION['selogeo']=true;
+
+      }
+  }
+}
+ if($_SESSION['selogeo']!=true){
+   $_SESSION['email']='';
+   $_SESSION['nombres']='';
+   $_SESSION['selogeo']='';
+ }
+  if(isset($_POST['mantenerme']) && $_SESSION['selogeo']==true){
+    setcookie('email', $email, time() + 60*60*24*30);
+    header('location:perfil.php');
+  }
+  else{
+    if($_SESSION['selogeo']==true){
+      header('location:perfil.php');
+    }
+  }
+}
+ ?>
 
     <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -15,7 +63,7 @@
   <body>
     <form class="" action="" method="post">
       <h2>Inicie sesión o Registrese</h2>
-      <input type="text" placeholder="&#128272; Usuario" name="Usuario">
+      <input type="email" placeholder="&#128272; email" name="Email">
       <input type="password" placeholder="&#128272; password" name="password">
       <input type="submit" value="Ingresar">
 
