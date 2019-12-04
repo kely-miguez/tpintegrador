3212e40ba0b951fb98f7ebadc706a740aec727dc
roni <roni.v205@gmail.com>
2019-12-04 12:53:19 -0300

    cambios de contacto

--- contacto.php
+++ contacto.php
@@ -1,56 +1,136 @@
- <!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="CSS/tp1.css">
-    <title>formulario</title>
-  </head>
-  <body>
-<div class="">
+<?php
+$errores = [];
+if ($_POST) {
+	$firstname = $_POST['firstname'];
+	$lastname = $_POST['lastname'];
+	$email = $_POST['email'];
+  $website = $_POST['website'];
+  $message = $_POST['message'];
+	if (empty($firstname)) {
+		$errores['firstname'] = 'El campo nombre esta vacio..';
+	}
+	if (empty($lastname)) {
+		$errores['lastname'] = 'El campo apellido esta vacio..';
+	}
+	if (empty($email)) {
+		$errores['email'] = 'El campo email esta vacio..';
+	}
+	if (empty($website)) {
+		$errores['website'] = 'El campo numero esta vacio..';
+	}
+	if(empty($message)){
+		$errores['message'] = 'El campo mensaje esta vacio..';
+	}
+	if (!$errores) {
+	 echo "todo ok!";
+	}
+}
+ ?>
+ <!doctype html>
+ <html class="no-js" lang="zxx">
+ <head>
+ 	<meta charset="utf-8">
+ 	<meta http-equiv="x-ua-compatible" content="ie=edge">
+ 	<title>Contacto /Dayross</title>
+ 	<meta name="description" content="">
+ 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
+  <link rel="stylesheet" href="perfil.php">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="CSS/stylos.css">
+  <link rel="stylesheet" href="CSS/login.css">
+ </head>
+ <body>
+ 	<div class="wrapper" id="wrapper">
 
-    <form class="" action="index.html" method="post" enctype="multipart/form-data">
-      <h1>CONTACTO</h1>
-      <p>
-        <label for="Nombre">
+    <?php
+    require_once('plantilla/header.php');
+     ?>
+         <div class="ht__bradcaump__area bg-image--6">
+             <div class="container">
+                 <div class="row">
+                     <div class="col-lg-12">
+                         <div class="bradcaump__inner text-center">
+                         	<h2 class="bradcaump-title">Contacto</h2>
+                             <nav class="bradcaump-content">
+                               <a class="breadcrumb_item" href="index.php">Home</a>
+                               <span class="brd-separetor">/</span>
+                               <span class="breadcrumb_item active">Contacto</span>
+                             </nav>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+         <section class="wn_contact_area bg--white pt--80 pb--80">
 
-          <input id ="Nombre" type="text" name="Nombre" value="" placeholder="NOMBRE">
-        </label>
-      </p>
-      <p>
-        <label for="Numero">
+         	<div class="container">
+         		<div class="row">
+         			<div class="col-lg-8 col-12">
+         				<div class="contact-form-wrap">
+         					<h2 class="contact__title">Mensaje.</h2>
 
-          <input id = "numero" type="text" name="numero" value="" placeholder="NUMERO">
-        </label>
-      </p>
-      <p>
-        <label for="Email">
+                             <form id="contact-form" action="#" method="post">
+                                 <div class="single-contact-form space-between">
+ 																	<input type="text" value='<?=$firstname ?? '' ?>' name="firstname" placeholder="Nombre*">
+ 																	<small style="color : red;"><?=$errores['firstname'] ?? '' ?></small>
+ 																	<input type="text" value='<?=$lastname ?? '' ?>' name="lastname" placeholder="Apellido*">
+ 																		<small style="color : red;"><?=$errores['lastname'] ?? '' ?></small>
+                                 </div>
+                                 <div class="single-contact-form space-between">
+ 																	<input type="email" value='<?=$email ?? '' ?>' name="email" placeholder="Email*">
+ 																		<small style="color : red;"><?=$errores['email'] ?? '' ?></small>
+ 																	<input type="text" value='<?=$website ?? '' ?>' name="website" placeholder="telefono*">
+ 																		<small style="color : red;"><?=$errores['website'] ?? '' ?></small>
+                                 </div>
 
-          <input id ="Email" type="email" name="email" value="" placeholder="EMAIL">
-        </label>
-        <p>
-          <label for="Mensaje">
-
-            <textarea name="Mensaje" class="form-textarea" placeholder="MENSAJE"></textarea>
-          </label>
-        </p>
-
-      <p>
-        <input type="submit" name="" value="ENVIAR" id = "boton">
-      </p>
-      <div class="">
-        <label for="">Cv</label>
-        <input type="file" name="cv" value="">
-  </div>
-      <div class="">
-        <label for="">Foto de Perfil</label>
-        <input type="file" name="foto de perfil" value="">
-      </div>
-     <div class="">
-       <input type="submit" name="" value="Enviar">
-     </div>
-
-    </form>
-</div>
-  </body>
-</html>
+                                 <div class="single-contact-form message">
+ 																	<textarea type ="text" name="message" value='<?=$message ?? '' ?>' placeholder="Dejanos tu mensaje..."></textarea>
+ 																			<small style="color : red;"><?=$errores['message'] ?? '' ?></small>
+                                 </div>
+                                 <div class="contact-btn">
+                                     <button type="submit">Enviar</button>
+                                 </div>
+                             </form>
+                         </div>
+                         <div class="form-output">
+                             <p class="form-messege">
+                         </div>
+         			</div>
+         			<div class="col-lg-4 col-12 md-mt-40 sm-mt-40">
+         				<div class="wn__address">
+         					<h2 class="contact__title">Contactanos.</h2>
+         					<div class="wn__addres__wreapper">
+         						<div class="single__address">
+         							<i class="icon-location-pin icons"></i>
+         							<div class="content">
+         								<span>Direccion:</span>
+         								<p>Bogota 3264 , Local : 44</p>
+         							</div>
+         						</div>
+         						<div class="single__address">
+         							<i class="icon-phone icons"></i>
+         							<div class="content">
+         								<span>telefono:</span>
+         								<p>1158904175</p>
+         							</div>
+         						</div>
+         						<div class="single__address">
+         							<i class="icon-envelope icons"></i>
+         							<div class="content">
+         								<span>Email:</span>
+         								<p>dayross@gmail.com</p>
+         							</div>
+         						</div>
+         					</div>
+         				</div>
+         			</div>
+         		</div>
+         	</div>
+         </section>
+         <?php
+         require_once('plantilla/footer.php');
+          ?>
+ </body>
+ </html>
--- plantilla/header.php
+++ plantilla/header.php
@@ -1,17 +1,12 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <title>Dayross</title>
-  <link href="https://fonts.googleapis.com/css?family=Montserrat:200" rel="stylesheet">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<header id="wn__header" class="oth-page header__area header__absolute sticky__header">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-4 col-sm-4 col-7 col-lg-2">
+          <div class="logo">
+            <a href="index.php">
+              <img src="https://dewey.tailorbrands.com/production/brand_version_mockup_image/318/2342459318_d7484af5-64db-4947-976b-1069df3db2a5.png?cb=1571669948" alt="logo images">
+            </a>
+          </div>
+        </div>
 
-    <link rel="stylesheet" href="CSS/stylos.css">
-    <link rel="stylesheet" href="CSS/home.css">
-
-</head>
-<body>
-
-  <div class="container">
+  </header>
